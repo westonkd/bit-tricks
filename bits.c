63dc71149ef4631e5254ecc88e1d1b5116451859
westonkd <wdrans@gmail.com>
2016-01-21 09:24:35 -0700

    first commit

--- bits.c
+++ bits.c
@@ -8,9 +8,9 @@
 *    descriptive text
 ***********************************************************************/
 
-/* 
- * CS:APP Data Lab 
- * 
+/*
+ * CS:APP Data Lab
+ *
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
  *          Even though you may work as a team, all students should
@@ -20,7 +20,7 @@
  * compiler. You can still use printf for debugging without including
  * <stdio.h>, although you might get a compiler warning. In general,
  * it's not good practice to ignore compiler warnings, but in this
- * case it's OK.  
+ * case it's OK.
  */
 
 #include "btest.h"
@@ -38,17 +38,17 @@ team_struct team =
       or, ID1+ID2 where ID1 is the login ID of the first team member
       and ID2 is the login ID of the second team member.
        Example: joestudent+zmename */
-    "", 
+    "westonkd+",
    /* Student name 1: Replace with the name of first team member */
-   "",
+   "Weston Dransfield",
    /* Login ID 1: Replace with the login ID of first team member */
-   "",
+   "westonkd",
 
    /* The following should only be changed if there are two team members */
    /* Student name 2: Name of the second team member */
-   "",
+   "Kevin",
    /* Login ID 2: Login ID of the second team member */
-   ""
+   "?"
 };
 
 #if 0
@@ -60,11 +60,11 @@ You will provide your solution to the Data Lab by
 editing the collection of functions in this source file.
 
 CODING RULES:
- 
+
   Replace the "return" statement in each function with one
-  or more lines of C code that implements the function. Your code 
+  or more lines of C code that implements the function. Your code
   must conform to the following style:
- 
+
   int Funct(arg1, arg2, ...) {
       /* brief description of how your implementation works */
       int var1 = Expr1;
@@ -90,11 +90,11 @@ CODING RULES:
   Each "Expr" is an expression using ONLY the following:
   1. Integer constants 0 through 255 (0xFF), inclusive. You are
       not allowed to use big constants such as 0xffffffff.
-     NOTE: You may create big constants using code.  
+     NOTE: You may create big constants using code.
   2. Function arguments and local variables (no global variables).
   3. Unary integer operations ! ~
   4. Binary integer operations & ^ | + << >>
-    
+
   Some of the problems restrict the set of allowed operators even further.
   Each "Expr" may consist of multiple operators. You are not restricted to
   one operator per line.
@@ -108,7 +108,7 @@ CODING RULES:
   4. Call any functions.
   5. Use any other operations, such as &&, ||, -, or ?:
   6. Use any form of casting.
- 
+
   You may assume that your machine:
   1. Uses 2s complement, 32-bit representations of integers.
   2. Performs right shifts arithmetically.
@@ -136,64 +136,69 @@ EXAMPLES OF ACCEPTABLE CODING STYLE:
 
 
 NOTES:
-  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+  1. Use the dlc (data lab checker) compiler (described in the handout) to
      check the legality of your solutions.
   2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
-     that you are allowed to use for your implementation of the function. 
-     The max operator count is checked by dlc. Note that '=' is not 
+     that you are allowed to use for your implementation of the function.
+     The max operator count is checked by dlc. Note that '=' is not
      counted; you may use as many of these as you want without penalty.
   3. Use the btest test harness to check your functions for correctness.
   4. The maximum number of ops for each function is given in the
-     header comment for each function. If there are any inconsistencies 
+     header comment for each function. If there are any inconsistencies
      between the maximum ops in the writeup and in this file, consider
      this file the authoritative source.
 #endif
 
 /*
  * STEP 3: Modify the following functions according the coding rules.
- * 
+ *
  *   IMPORTANT. TO AVOID GRADING SURPRISES:
  *   1. Use the dlc compiler to check that your solutions conform
  *      to the coding rules.
- *   2. Use the btest test harness to check that your solutions produce 
+ *   2. Use the btest test harness to check that your solutions produce
  *      the correct answers. Watch out for corner cases around Tmin and Tmax.
  */
-/* 
- * bitNor - ~(x|y) using only ~ and & 
+/*
+ * bitNor - ~(x|y) using only ~ and &
  *   Example: bitNor(0x6, 0x5) = 0xFFFFFFF8
  *   Legal ops: ~ &
  *   Max ops: 8
  *   Rating: 1
+ * Returns ~x & ~y
  */
 int bitNor(int x, int y) {
-  return 2;
+   //simply DeMorganize ~(x|y) to get ~x & ~y
+  return ~x & ~y;
 }
-/* 
- * bitXor - x^y using only ~ and & 
+/*
+ * bitXor - x^y using only ~ and &
  *   Example: bitXor(4, 5) = 1
  *   Legal ops: ~ &
  *   Max ops: 14
  *   Rating: 2
+ *
+ * XOR = (x or y) and ~(x and y)
+ *     = ~(~x and ~y) and ~(x and y)
  */
 int bitXor(int x, int y) {
-
-
-
-
-  return 2;
-
+   //Use the above bitNor (notted) anded with 'not x and y'.
+  return ~(~x & ~y) & ~(x & y);
 }
-/* 
- * isNotEqual - return 0 if x == y, and 1 otherwise 
+/*
+ * isNotEqual - return 0 if x == y, and 1 otherwise
  *   Examples: isNotEqual(5,5) = 0, isNotEqual(4,5) = 1
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 6
  *   Rating: 2
+ *
+ *  Any number XOR itself is always 0. We just need to use two !! in order
+ *  to turn the return value into a 1 or 0.
  */
 int isNotEqual(int x, int y) {
-  return 2;
+   //x XOR y equals zero if x and y are equal
+  return !!(x ^ y);
 }
-/* 
+/*
  * getByte - Extract byte n from word x
  *   Bytes numbered from 0 (LSB) to 3 (MSB)
  *   Examples: getByte(0x12345678,1) = 0x56
@@ -204,7 +209,7 @@ int isNotEqual(int x, int y) {
 int getByte(int x, int n) {
   return 2;
 }
-/* 
+/*
  * copyLSB - set all bits of result to least significant bit of x
  *   Example: copyLSB(5) = 0xFFFFFFFF, copyLSB(6) = 0x00000000
  *   Legal ops: ! ~ & ^ | + << >>
@@ -214,13 +219,13 @@ int getByte(int x, int n) {
 int copyLSB(int x) {
   return 2;
 }
-/* 
+/*
  * logicalShift - shift x to the right by n, using a logical shift
  *   Can assume that 1 <= n <= 31
  *   Examples: logicalShift(0x87654321,4) = 0x08765432
  *   Legal ops: ~ & ^ | + << >>
  *   Max ops: 16
- *   Rating: 3 
+ *   Rating: 3
  */
 int logicalShift(int x, int n) {
   return 2;
@@ -235,29 +240,29 @@ int logicalShift(int x, int n) {
 int bitCount(int x) {
   return 2;
 }
-/* 
+/*
  * bang - Compute !x without using !
  *   Examples: bang(3) = 0, bang(0) = 1
  *   Legal ops: ~ & ^ | + << >>
  *   Max ops: 12
- *   Rating: 4 
+ *   Rating: 4
  */
 int bang(int x) {
   return 2;
 }
-/* 
+/*
  * leastBitPos - return a mask that marks the position of the
  *               least significant 1 bit. If x == 0, return 0
  *   Example: leastBitPos(96) = 0x20
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 6
- *   Rating: 4 
+ *   Rating: 4
  */
 int leastBitPos(int x) {
   return 2;
 }
-/* 
- * TMax - return maximum two's complement integer 
+/*
+ * TMax - return maximum two's complement integer
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 4
  *   Rating: 1
@@ -265,8 +270,8 @@ int leastBitPos(int x) {
 int tmax(void) {
   return 2;
 }
-/* 
- * isNonNegative - return 1 if x >= 0, return 0 otherwise 
+/*
+ * isNonNegative - return 1 if x >= 0, return 0 otherwise
  *   Example: isNonNegative(-1) = 0.  isNonNegative(0) = 1.
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 6
@@ -275,8 +280,8 @@ int tmax(void) {
 int isNonNegative(int x) {
   return 2;
 }
-/* 
- * isGreater - if x > y  then return 1, else return 0 
+/*
+ * isGreater - if x > y  then return 1, else return 0
  *   Example: isGreater(4,5) = 0, isGreater(5,4) = 1
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 24
@@ -285,7 +290,7 @@ int isNonNegative(int x) {
 int isGreater(int x, int y) {
   return 2;
 }
-/* 
+/*
  * divpwr2 - Compute x/(2^n), for 0 <= n <= 30
  *  Round toward zero
  *   Examples: divpwr2(15,1) = 7, divpwr2(-33,4) = -2
@@ -296,7 +301,7 @@ int isGreater(int x, int y) {
 int divpwr2(int x, int n) {
     return 2;
 }
-/* 
+/*
  * abs - absolute value of x (except returns TMin for TMin)
  *   Example: abs(-1) = 1.
  *   Legal ops: ! ~ & ^ | + << >>
@@ -306,10 +311,10 @@ int divpwr2(int x, int n) {
 int abs(int x) {
   return 2;
 }
-/* 
+/*
  * addOK - Determine if can compute x+y without overflow
  *   Example: addOK(0x80000000,0x80000000) = 0,
- *            addOK(0x80000000,0x70000000) = 1, 
+ *            addOK(0x80000000,0x70000000) = 1,
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 20
  *   Rating: 3
